2f6319718fbb59b7eb1ff32a1b528e19544ed032
HUY NGUYEN <phanhuy1502@gmail.com>
2017-07-02 02:35:45 +0700

    add svm and naivebayes classifier

--- CODE/classifiers/decision_tree.py
+++ CODE/classifiers/decision_tree.py
@@ -1,9 +1,7 @@
-from numpy import genfromtxt
 from utils import *
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report
-
-match_result = genfromtxt('match_player_result.csv', delimiter=',', dtype=int, skip_header=1)
+from sklearn.metrics import confusion_matrix
 
 train_X = get_attributes('train_binary.csv')
 train_Y = get_classes('train_binary.csv')
@@ -14,8 +12,6 @@ test_Y = get_classes('test_binary.csv')
 tree_benchmark = DecisionTreeClassifier(max_leaf_nodes=50, random_state=0)
 tree_benchmark.fit(train_X, train_Y)
 y_pred_benchmark = tree_benchmark.predict(test_X)
-print (y_pred_benchmark)
-print(classification_report(test_Y, y_pred_benchmark))
 
-# y_pred_benchmark = tree_benchmark.predict(train_X)
-# print(classification_report(train_Y, y_pred_benchmark))
+print(classification_report(test_Y, y_pred_benchmark))
+print(confusion_matrix(test_Y, y_pred_benchmark))
--- CODE/classifiers/naivebayes.py
+++ CODE/classifiers/naivebayes.py
@@ -1,5 +1,5 @@
 from utils import *
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
@@ -9,10 +9,10 @@ train_Y = get_classes('train_binary.csv')
 test_X = get_attributes('test_binary.csv')
 test_Y = get_classes('test_binary.csv')
 
-neigh = KNeighborsClassifier(n_neighbors=10)
+clf = GaussianNB()
 
-neigh.fit(train_X, train_Y)
-y_pred_benchmark = neigh.predict(test_X)
+clf.fit(train_X, train_Y)
+y_pred_benchmark = clf.predict(test_X)
 
 print(classification_report(test_Y, y_pred_benchmark))
 print(confusion_matrix(test_Y, y_pred_benchmark))
